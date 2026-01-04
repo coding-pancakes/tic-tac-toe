c9e52070221ed2223a13806c96ba166fd355a5d5
giannag27 <giacosey28@gmail.com>
2026-01-04 17:10:07 -0500

    Fixed missing line

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -9,7 +9,8 @@ function printBoard() {
         ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
        ------------
         ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
-       -----------
+       ------------
         ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
         `);
 }
+
