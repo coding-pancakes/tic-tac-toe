48448ff779e000fc077193cb808061fd201d8db9
giannag27 <giacosey28@gmail.com>
2026-01-04 17:09:23 -0500

    Created board placeholder function

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -4,3 +4,12 @@ let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 let currentPlayer = "☕️";
 let gameActive = true;
 
+function printBoard() {
+    console.log(`
+        ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
+       ------------
+        ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
+       -----------
+        ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
+        `);
+}
